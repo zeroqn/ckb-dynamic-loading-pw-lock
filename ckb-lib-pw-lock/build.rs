be2fbd2ea89f186128aae6388bee206c65d44488
zero.qn <zero.qn@outlook.com>
2020-12-15 18:59:29 +0800

    feat: also expose secp256k1 data

--- ckb-lib-pw-lock/build.rs
+++ ckb-lib-pw-lock/build.rs
@@ -8,6 +8,7 @@ const BUF_SIZE: usize = 8 * 1024;
 const CKB_HASH_PERSONALIZATION: &[u8] = b"ckb-default-hash";
 
 const PW_LOCK_CELL_PATH: &str = "../pw-lock/specs/cells";
+const PW_LOCK_SECP256K1_DATA: &str = "secp256k1_data";
 const PW_LOCK_SECP256K1_KECCAK256: &str = "secp256k1_keccak256_sighash_all";
 const PW_LOCK_SECP256K1_KECCAK256_ACPL: &str = "secp256k1_keccak256_sighash_all_acpl";
 
@@ -47,6 +48,7 @@ fn main() {
     let mut out_file = BufWriter::new(File::create(&out_path).expect("create code_hashes.rs"));
 
     let cells = vec![
+        ("SECP256K1_DATA", PW_LOCK_SECP256K1_DATA),
         ("SECP256K1_KECCAK256", PW_LOCK_SECP256K1_KECCAK256),
         ("SECP256K1_KECCAK256_ACPL", PW_LOCK_SECP256K1_KECCAK256_ACPL),
     ];
--- ckb-lib-pw-lock/src/lib.rs
+++ ckb-lib-pw-lock/src/lib.rs
@@ -5,5 +5,5 @@ extern crate alloc;
 mod code_hashes;
 mod pw_lock;
 
-pub use code_hashes::{SECP256K1_KECCAK256, SECP256K1_KECCAK256_ACPL};
+pub use code_hashes::{SECP256K1_DATA_BIN, SECP256K1_KECCAK256, SECP256K1_KECCAK256_ACPL};
 pub use pw_lock::PWLockAcpl;
