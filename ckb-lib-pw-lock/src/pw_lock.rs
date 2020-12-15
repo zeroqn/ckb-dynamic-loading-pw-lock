cd9aa11a5a830a7b68f4735c1a4d6ba2cc4a085a
zero.qn <zero.qn@outlook.com>
2020-12-15 16:34:35 +0800

    feat: add verify_secp256k1_keccak_sighash_all to ckb-lib-pw-lock

--- ckb-lib-pw-lock/src/pw_lock.rs
+++ ckb-lib-pw-lock/src/pw_lock.rs
@@ -2,12 +2,15 @@ use ckb_std::dynamic_loading::{CKBDLContext, Symbol};
 
 use crate::code_hashes::SECP256K1_KECCAK256_ACPL;
 
+type VerifySecp256k1KeccakSighashAll = unsafe extern "C" fn(eth_address: *const u8) -> i32;
 type Main = unsafe extern "C" fn() -> i32;
 
+const VERIFY_SECP256K1_KECCAK_SIGHASH_ALL: &[u8; 35] = b"verify_secp256k1_keccak_sighash_all";
 const MAIN: &[u8; 4] = b"main";
 
 pub struct PWLockAcpl {
     main: Symbol<Main>,
+    verify_secp256k1_keccak_sighash_all: Symbol<VerifySecp256k1KeccakSighashAll>,
 }
 
 impl PWLockAcpl {
@@ -16,14 +19,31 @@ impl PWLockAcpl {
             .load(&SECP256K1_KECCAK256_ACPL)
             .expect("load pw-lock secp256k1_keccak256_acpl");
 
+        let verify_secp256k1_keccak_sighash_all: Symbol<VerifySecp256k1KeccakSighashAll> = unsafe {
+            bin.get(VERIFY_SECP256K1_KECCAK_SIGHASH_ALL)
+                .expect("load verify function")
+        };
+
         let main: Symbol<Main> = unsafe { bin.get(MAIN).expect("load main function") };
 
-        PWLockAcpl { main }
+        PWLockAcpl {
+            main,
+            verify_secp256k1_keccak_sighash_all,
+        }
     }
 
-    pub fn verify(&self) -> Result<(), i32> {
-        let main = &self.main;
-        let error_code = unsafe { main() };
+    pub fn main(&self) -> Result<(), i32> {
+        let f = &self.main;
+        let error_code = unsafe { f() };
+        if error_code != 0 {
+            return Err(error_code);
+        }
+        Ok(())
+    }
+
+    pub fn verify(&self, eth_address: &mut [u8; 20]) -> Result<(), i32> {
+        let f = &self.verify_secp256k1_keccak_sighash_all;
+        let error_code = unsafe { f(eth_address.as_mut_ptr()) };
         if error_code != 0 {
             return Err(error_code);
         }
